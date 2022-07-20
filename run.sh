fe875190bc9be27702fada7e6b109d4b9b0771e4
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-07-20 17:35:32 -0500

    fixed depndcis hopefully

--- run.sh
+++ run.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 #Colin Brennan
 
+sudo apt-get update -y
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs   
 sudo apt-get install npm -y
 
 node -v
 npm -v
+npm install -g npm
 
 sudo npm i -g @nestjs/cli
-npm install -g npm
 npm install @nestjs/common
 npm install @nestjs/core
-npm install source-map-support
-npm install reflect-metadata
-npm install rxjs
-npm install @nestjs/platform-express
+
 
 cd rest-api/
 npm run start:dev
