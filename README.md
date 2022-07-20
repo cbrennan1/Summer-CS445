322017bb18c3ec2fa071fcee2a25503ea867e4f5
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-07-20 17:24:53 -0500

    testing run again... fixed readme a bit too

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Summer-CS445
 Colin Brennan
+
+This repository can be cloned to a target computer with the following command.
+```
+git clone https://github.com/cbrennan1/Summer-CS445
+```
+
+Once cloned the 'run.sh' script must have it's permissions changed before it can be run.
+```
+sudo chmod 777 run.sh
+./run.sh
+```
--- run.sh
+++ run.sh
@@ -4,13 +4,16 @@
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs   
 sudo apt-get install npm -y
+
 node -v
 npm -v
 
 sudo npm i -g @nestjs/cli
+npm install -g npm
 npm install @nestjs/common
 npm install @nestjs/core
 npm install source-map-support
+npm install reflect-metadata
 
 cd rest-api/
 npm run start:dev
