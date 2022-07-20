3e31e2d3ab21251d730ae36a6288531ec194d8e6
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-07-20 18:33:49 -0500

    notes

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AccountsModule } from './accounts/accounts.module';
 import { AsksModule } from './asks/asks.module';
 import { GivesModule } from './gives/gives.module';
+import { NotesModule } from './notes/notes.module';
 
 
 
 @Module({
-  imports: [AccountsModule, AsksModule, GivesModule],
+  imports: [AccountsModule, AsksModule, GivesModule, NotesModule],
   controllers: [],
   providers: [],
 })
