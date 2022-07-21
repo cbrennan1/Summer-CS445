e41b7f5a0f113ed3994245660f7e1be45f06a540
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-07-20 20:01:41 -0500

    reports generated/edited briefly

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -3,11 +3,12 @@ import { AccountsModule } from './accounts/accounts.module';
 import { AsksModule } from './asks/asks.module';
 import { GivesModule } from './gives/gives.module';
 import { NotesModule } from './notes/notes.module';
+import { ReportsModule } from './reports/reports.module';
 
 
 
 @Module({
-  imports: [AccountsModule, AsksModule, GivesModule, NotesModule],
+  imports: [AccountsModule, AsksModule, GivesModule, NotesModule, ReportsModule],
   controllers: [],
   providers: [],
 })
